465fd9931275f507a72eb2c9e4920251b7a0edd7
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-04-11 16:21:15 +0200

    Added function to cross items out

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 function newItem(){
 
+//adding a new item to the list
   let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
@@ -9,4 +10,14 @@ function newItem(){
     } else {
       $('#input').append(li);
     }
-    }
+
+//crossing an item out
+  function crossOut(){
+    li.toggleClass('strike');
+  }
+  li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+  });
+
+  //
+}
