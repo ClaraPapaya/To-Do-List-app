e6a99671eca235b559b9c15f856c294c81598bb9
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-04-11 16:06:07 +0200

    Added function to add new item

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function newItem(){
+
+  let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      $('#input').append(li);
+    }
+    }
